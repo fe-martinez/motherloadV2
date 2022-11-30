5837fbc88bf3575972914ad9b05659f9e521b27c
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-11-30 15:09:47 -0300

    Delete src/main/java/algo3/motherloadV2 directory

--- src/main/java/algo3/motherloadV2/Opcion.java
+++
@@ -1,6 +0,0 @@
-package algo3.motherloadV2;
-
-public enum Opcion {
-	JUGAR,
-	SALIR;
-}
--- src/main/java/algo3/motherloadV2/VistaEstacionDeReparacion.java
+++
@@ -1,8 +0,0 @@
-package algo3.motherloadV2;
-
-public class VistaEstacionDeReparacion {
-	//Es igual a la estación de servicio pero con otros valores :P
-	public static void mostrar() {
-		
-	}
-}
